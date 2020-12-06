61146db8a1f747a77560b153ebd8592b2f0fb2e1
Volodymyr Zahorodnyy <vovazahorodniy@gmail.com>
2020-12-05 21:01:12 -0800

    Updates

---
+++ precache-manifest.9c83d185b7c58b5e0df83f80389e36de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558628a368580578fdd5e5471ad8c4d7",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "662ce45ea0e647f00932",
+    "url": "/rock-paper-scissors/static/css/main.d3e07756.chunk.css"
+  },
+  {
+    "revision": "741f495e633c24095a1f",
+    "url": "/rock-paper-scissors/static/js/2.b0e71058.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors/static/js/2.b0e71058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "662ce45ea0e647f00932",
+    "url": "/rock-paper-scissors/static/js/main.a0f905c4.chunk.js"
+  },
+  {
+    "revision": "4152367e75813356d958",
+    "url": "/rock-paper-scissors/static/js/runtime-main.9b8d27fd.js"
+  },
+  {
+    "revision": "b9fa37f6f39a8201887597a7ab0cb374",
+    "url": "/rock-paper-scissors/static/media/paper.b9fa37f6.jpg"
+  },
+  {
+    "revision": "00b8386ac9fa24a634bbbd314dc3b3c6",
+    "url": "/rock-paper-scissors/static/media/rock.00b8386a.jpg"
+  },
+  {
+    "revision": "161404b09c83773be70da11a32c55b56",
+    "url": "/rock-paper-scissors/static/media/scissors.161404b0.jpg"
+  }
+]);
